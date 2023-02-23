bb689ec7d0ddae6768d315b66c478c469c0c1715
sudeshna2209 <125039157+sudeshna2209@users.noreply.github.com>
2023-02-23 20:27:48 +0530

    Add files via upload

---
+++ 5_list_methos.py
@@ -0,0 +1,94 @@
+'''Append()'''
+
+# a=[1,4,5.6,[21,34],'hello']
+# a.append(12)
+# print(a)
+
+# a=[2,6,9.0,'python',23]
+# a.append('hello')
+# print(a)
+
+'''clear()'''
+
+# a=[2,6,9.0,'python',23]
+# a.clear()
+# print(a)
+# print(type(a))
+
+'''count()'''
+
+# a=[12,23,34.5,12,34.5,'hello','',' ','']
+# print(a.count(12))
+# print(a.count(23))
+# print(a.count(''))
+# print(a.count(' '))
+# print(a.count(34.5))
+
+
+'''extend()'''
+
+# a=[1,2,3,4]
+# b=[2,6,8.5,'hello']
+# a.extend(b)
+# print(a)
+
+# b.extend(a)
+# print(b)
+
+
+'''index()'''
+# a=[2,3,4,6,8,[54],'hello',5.4]
+# # print(a.index([56]))  #valeerror:[56] is not in the list
+# print(a.index(4))
+# print(a.index('hello'))
+# print(a.index([54]))
+# print(a.index(5.4))
+
+'''insert()'''
+
+# a=[3,6.5,[45,67],'hello',21]
+# a.insert(1,43)
+# a.insert(-1,2.4)
+# a.insert(2,'python')
+# a.insert(3,4.9)
+# print(a)
+
+'''pop()'''
+
+# a=[10,20,30,40,50,60]
+# a.pop()
+# print(a)
+# a.pop(2)
+# print(a)
+# a.pop(1)
+# print(a)
+
+
+'''remove()'''
+
+# a=[1,2,3,4,5,6,7,8,9,10]
+# a.remove(8)
+# print(a)
+# a.remove('hello')
+# print(a)
+
+
+
+'''sort()'''
+
+# a=[20,45,67,43,23,13,9.2,3.0]
+# a.sort()
+# print(a)
+# a.sort(reverse=False)
+# print(a)
+# a.sort(reverse=True)
+# print(a)
+
+
+'''reverse()'''
+
+# a=[1,2,3,4,5]
+# print(a[::-1])
+# a.reverse()
+# print(a)
+
